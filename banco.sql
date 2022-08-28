ed0e39b7e0786e34d3101b05b271739b9966e85e
denilson-pinto <denilsonpinto89@gmail.com>
2022-08-28 01:51:44 -0300

    Mudança do banco de dados para o contexto de uma loja ou mercado

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dados (
-    AlunoID int,
+    ProdutoID int,
     Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
+    Descricao varchar(50),
+    Quantidade int,
+    Preco decimal(12,2),
     Host varchar(50)
 );
